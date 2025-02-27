63951eca2098d6d2a4e6713f4d3372af24149434
Ruben Boero <boeror2017@gmail.com>
2025-02-26 20:23:06 -0600

    Began working on Pipe object. Still needs random gap, collision with bird, and continuous spawning

--- game/flappy_bird.py
+++ game/flappy_bird.py
@@ -1,4 +1,9 @@
 import pygame
+import random
+
+GRAVITY = 5
+GAP_SIZE = 120
+PIPE_SCROLL_SPEED = 1.5
 
 # make bird class
 class Bird:
@@ -11,10 +16,26 @@ class Bird:
         self.radius = 25
         # i forgot one of the variables we talked about
 
+    def draw(self, window):
+        pygame.draw.circle(window, self.color, (self.x_coord, self.y_coord), self.radius)
+
     def flap(self):
         bird.velocity += 35
 
-# make obstacle class constructor 
+# # make obstacle class constructor 
+class Pipe:
+    def __init__(self, left, height):
+        # self.gap = some random choice for gap location
+        self.color = (75, 174, 78)
+        # Rect class parameters
+        self.left = left
+        self.top = 0
+        self.width = 85
+        self.height = height
+
+    def draw(self, window, window_height):
+        pygame.draw.rect(window, self.color, (self.left, self.top, self.width, window_height))
+        # still need to cut out the gap
 
 # ------------- game code -------------
 # setup window and basic game items
@@ -22,27 +43,28 @@ background_color = (5, 213, 250)
 window = pygame.display.set_mode((480, 640)) 
 
 pygame.display.set_caption('Bryce & Ruben: Flappy Bird') 
-  
-window.fill(background_color) 
-  
-pygame.display.update() 
 
 clock = pygame.time.Clock()
-
-gravity = 5
   
 running = True
 
 # setup bird object
 bird = Bird()
 
-# set screen position of bird
+# set initial screen position of bird
 w, h = pygame.display.get_surface().get_size()
 bird.x_coord = w / 2 - 50
 bird.y_coord = h / 2
 
+# initialize a single pipe object, will need to create more within while loop
+pipe = Pipe(w, h)
+
 # game loop 
 while running: 
+    # set FPS to 60
+    clock.tick(60)  
+    # https://stackoverflow.com/questions/35617246/setting-a-fixed-fps-in-pygame-python-3
+    # might want to look at this ^ later
     
 # for loop through the event queue   
     for event in pygame.event.get(): 
@@ -57,24 +79,23 @@ while running:
             elif event.key == pygame.K_q:
                 running = False
 
-    bird.velocity -= gravity
+    # update bird position
+    bird.velocity -= GRAVITY
     if bird.velocity <= 0:
         bird.velocity = 0
 
-    bird.y_coord += gravity - bird.velocity
+    bird.y_coord += GRAVITY - bird.velocity
 
     if bird.y_coord < 0 + bird.radius:
         bird.y_coord = bird.radius
-    print(bird.y_coord)
 
     if bird.y_coord >= h - bird.radius:
         running = False
 
-    window.fill(background_color)
-    pygame.draw.circle(window, bird.color, (bird.x_coord, bird.y_coord), bird.radius)
-    pygame.display.update()
+    # update pipe position
+    pipe.left -= PIPE_SCROLL_SPEED
 
-    # set FPS to 60
-    clock.tick(60)  
-    # https://stackoverflow.com/questions/35617246/setting-a-fixed-fps-in-pygame-python-3
-    # might want to look at this later
+    window.fill(background_color)
+    bird.draw(window)
+    pipe.draw(window, h)
+    pygame.display.update()
